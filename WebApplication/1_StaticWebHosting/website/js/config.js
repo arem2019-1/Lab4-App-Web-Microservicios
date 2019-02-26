e936ce907fb4cf9f0ea984a41fb456b883cc95e6
cesareduardo824 <2098325@linux-10.labinfo.is.escuelaing.edu.co>
2019-02-26 13:33:33 -0500

    FInal

---
+++ WebApplication/1_StaticWebHosting/website/js/config.js
@@ -0,0 +1,11 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_5SUQh4qjw', // us-east-1_5SUQh4qjw
+        userPoolClientId: '1fpfe4bqg1knb9760isbdohivj', // 1fpfe4bqg1knb9760isbdohivj
+        region: 'us-east-1' // us-east-1
+    },
+    api: {
+        invokeUrl: 'https://yet0e7am6e.execute-api.us-east-1.amazonaws.com/prod
+' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
